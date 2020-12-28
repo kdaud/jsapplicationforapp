2f2e6bb23cc305ec40eff7ec7efd583b5c883324
kdaud <dkakumirizii@gmail.com>
2020-12-28 15:50:07 +0300

    html api

--- sourcecode/student.js
+++ sourcecode/student.js
@@ -7,3 +7,22 @@ else{
     console.log("Invalid Entry");
 }
 console.log("Hello ");
+
+
+var organisation =[
+'OpenMRS',
+'JabRef',
+'Eclipse',
+'Amref',
+'ODK',
+'OpenSuse',
+'Mozilla'
+]
+
+console.log(organisation.push(3,"Spy","Firefox"));
+organisation.forEach(x => console.log(x));
+ var x,y;
+ x=22;
+ y=5;
+ 
+ console.log("The value is : "+x*y);
